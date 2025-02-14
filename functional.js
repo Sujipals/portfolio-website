ade9b98c51a5ff7b8e021888fd8fd2f3eeec4ee4
supa <sujithamar08@gmail.com>
2025-02-14 09:26:44 +0100

    changes

---
+++ functional.js
@@ -0,0 +1,10 @@
+
+    document.getElementById("https://www.linkedin.com/in/sujitha-palanivel-397489296").addEventListener("mouseover", function() {
+        this.style.color = "blue";
+        this.style.fontSize = "18px";
+    });
+
+    document.getElementById("https://www.linkedin.com/in/sujitha-palanivel-397489296").addEventListener("mouseout", function() {
+        this.style.color = "tomato";
+        this.style.fontSize = "10px";
+    });
